5a523b59b19a8f31b153b08dfef6d21466ce08d6
FrrBruno <bruno.f2005@aluno.ifsc.edu.br>
2025-03-17 09:42:25 -0300

    Commit Inicial

---
+++ app/src/main/java/br/com/brunoferrari/mobile/trabalho/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package br.com.brunoferrari.mobile.trabalho.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
